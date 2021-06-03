8848e0e61cee300a5d2dab882d0295df53251b2d
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-06-03 17:35:46 +0200

    Modified getCustomerQuery to also get orders

--- framework/shopify/utils/queries/get-customer-query.ts
+++ framework/shopify/utils/queries/get-customer-query.ts
@@ -1,16 +1,26 @@
 export const getCustomerQuery = /* GraphQL */ `
   query getCustomer($customerAccessToken: String!) {
-    customer(customerAccessToken: $customerAccessToken) {
-      id
-      firstName
-      lastName
-      displayName
-      email
-      phone
-      tags
-      acceptsMarketing
-      createdAt
+  customer(customerAccessToken: $customerAccessToken) {
+    id
+    firstName
+    lastName
+    displayName
+    email
+    phone
+    tags
+    acceptsMarketing
+    createdAt
+    orders(first: 10) {
+      edges {
+        node {
+          id
+          name
+          totalPrice
+          fulfillmentStatus
+        }
+      }
     }
   }
+}
 `
 export default getCustomerQuery
