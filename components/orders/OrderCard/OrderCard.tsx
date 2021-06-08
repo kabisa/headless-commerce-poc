ae61da0f8167a707e83f2df003cd5b940e94f949
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-06-08 21:52:02 +0200

    Added amount text style

--- components/orders/OrderCard/OrderCard.tsx
+++ components/orders/OrderCard/OrderCard.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react'
 import { OrderEdge } from "@framework/schema";
 import s from './OrderCard.module.css'
 import { capitalize } from "@lib/capitalize";
-import getProduct from "@framework/product/get-product";
 
 interface Props {
   order: OrderEdge
@@ -24,7 +23,7 @@ const OrderCard: FC<Props> = ({
       <div className={s.orderItem} key={product.node.variant?.id}>
         <a href={`/product/${product.node.variant?.product.handle}`}>
         <img className={s.itemImage} src={product.node.variant?.product.images.edges[0].node.transformedSrc}/>
-        <span className={s.variant}>{product.node.variant?.product.title}</span></a><span>: {product.node.quantity}x</span>
+        <span className={s.variant}>{product.node.variant?.product.title}</span></a><span className={s.amount}>: {product.node.quantity}x</span>
       </div>
     ))}
       </div>
