dabdfe7de54e93a6b7512083b8db3b4e2c78cde1
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-06-08 19:21:23 +0200

    Updated OrderCard component

--- components/orders/OrderCard/OrderCard.tsx
+++ components/orders/OrderCard/OrderCard.tsx
@@ -1,42 +1,33 @@
 import { FC } from 'react'
 import { OrderEdge } from "@framework/schema";
 import s from './OrderCard.module.css'
+import { capitalize } from "@lib/capitalize";
+import getProduct from "@framework/product/get-product";
 
 interface Props {
   order: OrderEdge
 }
-
-function getProductUrlFromTitle(title: string) { // Not the way! Have to get url from product
-  return `/product/${title.toLowerCase() // Lower casing
-    .replaceAll('/', ' ') // Replace spaces with dashes
-    .replaceAll(/[^a-zA-Z ,1-9-]/g, "") // Remove special chars
-    .replaceAll(' ', '-') // Replace spaces with dashes
-    .replaceAll('--', '-')}` // Replace double dashes with single dash
-}
-
-// async function getProductUrlFromId(id: string, config, preview) {
-  // const {product} = await getProduct({
-  //   variables: {slug: params!.slug},
-  //   config,
-  //   preview,
-  // })
-// }
 
 const OrderCard: FC<Props> = ({
   order,
   ...props
 }) => (
     <div className={s.orderCard}>
-    <h1 className={s.h1}>Order: {order.node.name}</h1>
-    <h2 className={s.h2}>Total: {order.node.totalPrice} {order.node.currencyCode}</h2>
-    <h3 className={s.h3}>Status: {order.node.fulfillmentStatus}</h3>
+    <h1 className={s.orderName}>Order: {order.node.name}</h1>
+    <p className={s.orderTotal}>Total: {order.node.totalPrice} {order.node.currencyCode}</p>
+    <p className={s.orderStatus}>Status: <span className={order.node.fulfillmentStatus.toLowerCase() == 'fulfilled' ? s.fulfilled : ''}>{capitalize(order.node.fulfillmentStatus)}</span></p>
+      <p className={s.orderDate}>{new Date(Date.parse(order.node.processedAt)).toDateString()}</p>
       <br/>
-      <h3 className={s.h3}>Items</h3>
+      <p className={s.orderProducts}>Items:</p>
+      <div className={s.orderItems}>
     {order.node.lineItems.edges && order.node.lineItems.edges.map((product) => (
-      <div key={product.node.title}>
-        <a href={getProductUrlFromTitle(product.node.variant!.product.title)}><span className={s.variant}>{product.node.title}</span></a><span>: {product.node.quantity}x</span>
+      <div className={s.orderItem} key={product.node.title}>
+        <a href={`/product/${product.node.variant?.product.handle}`}>
+        <img className={s.itemImage} src={product.node.variant?.product.images.edges[0].node.transformedSrc}/>
+        <span className={s.variant}>{product.node.variant?.product.title}</span></a><span>: {product.node.quantity}x</span>
       </div>
     ))}
+      </div>
   </div>
 )
 
