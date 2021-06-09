c6088bf504ab9d576d22135c7cd257fb6f41e9af
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-06-09 14:12:37 +0200

    Added ordercard items scrolling fade effect and styling

--- components/orders/OrderCard/OrderCard.tsx
+++ components/orders/OrderCard/OrderCard.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import React, { FC } from 'react'
 import { OrderEdge } from "@framework/schema";
 import s from './OrderCard.module.css'
 import { capitalize } from "@lib/capitalize";
@@ -10,24 +10,38 @@ interface Props {
 const OrderCard: FC<Props> = ({
   order,
   ...props
-}) => (
-    <div className={s.orderCard}>
+}) => {
+
+  const handleScroll = (event: React.UIEvent<HTMLElement>) => {
+    const target = event.target! as HTMLElement
+    const scrolled = target.scrollTop === (target.scrollHeight - target.offsetHeight)
+
+    scrolled ? target.classList.add(s.atBottom) : target.classList.remove(s.atBottom);
+  };
+
+  return (
+  <div className={s.orderCard}>
     <h1 className={s.orderName}>Order: {order.node.name}</h1>
     <p className={s.orderTotal}>Total: {order.node.totalPriceV2.amount} {order.node.totalPriceV2.currencyCode}</p>
-    <p className={s.orderStatus}>Status: <span className={order.node.fulfillmentStatus.toLowerCase() == 'fulfilled' ? s.fulfilled : ''}>{capitalize(order.node.fulfillmentStatus)}</span></p>
-      <p className={s.orderDate}>{new Date(Date.parse(order.node.processedAt)).toDateString()}</p>
-      <br/>
-      <p className={s.orderProducts}>Items:</p>
-      <div className={s.orderItems}>
-    {order.node.lineItems.edges && order.node.lineItems.edges.map((product) => (
-      <div className={s.orderItem} key={product.node.variant?.id}>
-        <a href={`/product/${product.node.variant?.product.handle}`}>
-        <img className={s.itemImage} src={product.node.variant?.product.images.edges[0].node.transformedSrc}/>
-        <span className={s.variant}>{product.node.variant?.product.title}</span></a><span className={s.amount}>: {product.node.quantity}x</span>
-      </div>
-    ))}
-      </div>
+    <p className={s.orderStatus}>Status: <span
+      className={order.node.fulfillmentStatus.toLowerCase() == 'fulfilled' ? s.fulfilled : ''}>{capitalize(order.node.fulfillmentStatus)}</span>
+    </p>
+    <p className={s.orderDate}>{new Date(Date.parse(order.node.processedAt)).toDateString()}</p>
+    <br/>
+    <p className={s.orderProducts}>Items:</p>
+    <div className={s.orderItems} onScroll={handleScroll}>
+      {order.node.lineItems.edges && order.node.lineItems.edges.map((product) => (
+        <div className={s.orderItem} key={product.node.variant?.id}>
+          <a href={`/product/${product.node.variant?.product.handle}`}>
+            <img className={s.itemImage} src={product.node.variant?.product.images.edges[0].node.transformedSrc}/>
+            <span className={s.variant}>{product.node.variant?.product.title}</span></a><span
+          className={s.amount}>: {product.node.quantity}x</span>
+        </div>
+      ))}
+      {order.node.lineItems.edges.length > 3 && <div className={s.fadeout}/>}
+    </div>
   </div>
-)
+  )
+}
 
 export default OrderCard
