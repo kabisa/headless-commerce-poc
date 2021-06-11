9abf088cabab005bdb912f3e237f75b44c29c1f2
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-06-11 16:26:04 +0200

    Created LogoutView.tsx

---
+++ components/auth/LogoutView.tsx
@@ -0,0 +1,20 @@
+import { FC} from 'react'
+import { Logo, Input } from '@components/ui'
+
+const LogoutView: FC = () => {
+  return (
+    <div
+      className="w-80 flex flex-col justify-between p-3"
+    >
+      <div className="flex justify-center pb-12 ">
+        <Logo width="64px" height="64px" />
+      </div>
+      <div className="flex flex-col space-y-3">
+          <span className="text-center text-xl">Successfully logged out!</span>
+        <Input type="email" placeholder="Email" className="h-0 opacity-0"/>
+      </div>
+    </div>
+  )
+}
+
+export default LogoutView
