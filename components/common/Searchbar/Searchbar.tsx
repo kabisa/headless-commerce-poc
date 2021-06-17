15eba5d6bd3eb61ca22b87d47fc4456a4b1bf8f7
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-06-17 17:27:56 +0200

    Changed iconContainer tag to button

--- components/common/Searchbar/Searchbar.tsx
+++ components/common/Searchbar/Searchbar.tsx
@@ -49,7 +49,7 @@ const Searchbar: FC<Props> = ({ className, id = 'search' }) => {
             }
           }}
         />
-        <div
+        <button
           className={s.iconContainer}
           onClick={() => {
             doQuery((document.getElementById(id) as HTMLInputElement).value)
@@ -62,7 +62,7 @@ const Searchbar: FC<Props> = ({ className, id = 'search' }) => {
               d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
             />
           </svg>
-        </div>
+        </button>
       </div>
     ),
     []
