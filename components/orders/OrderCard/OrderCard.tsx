d267e9731aac44e2e34aea31ff2ae6f5c235ace5
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-06-17 15:09:35 +0200

    Added subtotal and shipping price to order card. Added parsing of price amounts and fade in/out elements for scrollable orders (> 3 orders)

--- components/orders/OrderCard/OrderCard.tsx
+++ components/orders/OrderCard/OrderCard.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react'
 import { OrderEdge } from "@framework/schema";
 import s from './OrderCard.module.css'
 import { capitalize } from "@lib/capitalize";
+import { displayAmount } from "@lib/displayAmount";
 
 interface Props {
   order: OrderEdge
@@ -17,21 +18,27 @@ const OrderCard: FC<Props> = ({
   const handleScroll = (event: React.UIEvent<HTMLElement>) => {
     const target = event.target as HTMLElement
     const scrolledToBottom = target.scrollTop === (target.scrollHeight - target.offsetHeight)
+    const scrolledToTop = target.scrollTop === 0
 
     scrolledToBottom ? target.classList.add(s.atBottom) : target.classList.remove(s.atBottom)
+    scrolledToTop ? target.classList.add(s.atTop) : target.classList.remove(s.atTop)
   };
 
   return (
   <div className={s.orderCard}>
     <h1 className={s.orderName}>Order: {order.node.name}</h1>
-    <p className={s.orderTotal}>Total: {order.node.totalPriceV2.amount} {order.node.totalPriceV2.currencyCode}</p>
+    <p className={s.orderSubTotal}>Subtotal: {displayAmount(order.node.subtotalPriceV2?.amount)} {order.node.subtotalPriceV2?.currencyCode}</p>
+    <p className={s.orderShipping}>Shipping: { order.node.totalShippingPriceV2.amount !== "0.0" ? ( `${displayAmount(order.node.totalShippingPriceV2.amount)} ${order.node.totalShippingPriceV2.currencyCode}` ) : <span className={s.freeShipping}>Free</span> }</p>
+    <hr className={s.ruler}/>
+    <p className={s.orderTotal}>Total: {displayAmount(order.node.totalPriceV2.amount)} {order.node.totalPriceV2.currencyCode}</p>
     <p className={s.orderStatus}>Status: <span
       className={order.node.fulfillmentStatus.toLowerCase() == 'fulfilled' ? s.fulfilled : ''}>{capitalize(order.node.fulfillmentStatus)}</span>
     </p>
     <p className={s.orderDate}>{new Date(Date.parse(order.node.processedAt)).toDateString()}</p>
     <br/>
-    <p className={s.orderProducts}>Items:</p>
-    <div className={s.orderItems} onScroll={handleScroll}>
+    <p className={s.orderProducts}>You bought:</p>
+    <div className={`${s.orderItems} ${s.atTop}`} onScroll={handleScroll}>
+      {order.node.lineItems.edges.length > 3 && <section className={s.fadein}/>}
       {order.node.lineItems.edges && order.node.lineItems.edges.map((product) => (
         <div className={s.orderItem} key={product.node.variant?.id}>
           <a href={`/product/${product.node.variant?.product.handle || ''}`}>
@@ -39,7 +46,7 @@ const OrderCard: FC<Props> = ({
             <span className={s.variant}>{product.node.variant?.product.title}</span></a><span className={s.amount}>: {product.node.quantity}x</span>
         </div>
       ))}
-      {order.node.lineItems.edges.length > 3 && <div className={s.fadeout}/>}
+      {order.node.lineItems.edges.length > 3 && <section className={s.fadeout}/>}
     </div>
   </div>
   )
