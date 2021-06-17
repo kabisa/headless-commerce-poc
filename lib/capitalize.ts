dd52082c9689bcd70b086b30e61d9cbf13e53092
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-06-17 15:10:17 +0200

    Updated capitalize function to modern js syntax

--- lib/capitalize.ts
+++ lib/capitalize.ts
@@ -2,6 +2,6 @@
  * Function to convert string to lowercase and capitalized
  * @param value string value to convert
  */
-export function capitalize(value: string) {
+export const capitalize = (value: string): string => {
   return value.charAt(0).toUpperCase() + value.toLowerCase().slice(1);
 }
