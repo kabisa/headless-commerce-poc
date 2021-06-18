6af22bbefb52abacb782fcd8585546f0fde0c7a1
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-06-18 17:53:02 +0200

    Added primary color meta tag, derived from css to increase PWA lighthouse score

--- components/common/Head/Head.tsx
+++ components/common/Head/Head.tsx
@@ -1,15 +1,26 @@
-import { FC } from 'react'
+import {FC, useEffect, useState} from 'react'
 import NextHead from 'next/head'
-import { DefaultSeo } from 'next-seo'
+import {DefaultSeo} from 'next-seo'
 import config from '@config/seo.json'
+import tailwindConfig from 'tailwind.config'
 
 const Head: FC = () => {
+  const [primaryColor, setPrimaryColor]: any | null = useState(null)
+
+  useEffect(() => {
+    // Get the root element
+    const root = document.querySelector(':root');
+    const rs = getComputedStyle(root!);
+    setPrimaryColor(rs.getPropertyValue('--primary'))
+  }, [])
+
   return (
     <>
       <DefaultSeo {...config} />
       <NextHead>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="manifest" href="/site.webmanifest" key="site-manifest"/>
+        <meta name="theme-color" content={primaryColor}/>
       </NextHead>
     </>
   )
