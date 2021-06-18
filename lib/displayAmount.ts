5fd9ff4260dd0c70cbb0c73fed36c322fde23183
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-06-18 13:57:49 +0200

    Changed displayAmount method to format the following "12 converts to 12,-"

--- lib/displayAmount.ts
+++ lib/displayAmount.ts
@@ -1,8 +1,9 @@
 /**
  * Function to convert string amount to string with two decimals
- * Eg: "12.1" converts to "12.10"
+ * Eg: "12.1" converts to "12.10" "12 converts to 12,-"
  * @param amount string value to convert
  */
 export const displayAmount = (amount: string): string => {
-  return parseFloat(amount).toFixed(2)
+  let formattedAmount: number = parseFloat(amount)
+  return formattedAmount % 1 === 0 ? `${formattedAmount},-` : formattedAmount.toFixed(2);
 }
