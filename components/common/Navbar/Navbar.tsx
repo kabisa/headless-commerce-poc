ca77c00e31ea57f71fb46f72cd1ab188ca7f847f
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-06-22 15:41:24 +0200

    Add dynamically active menu items

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -1,50 +1,59 @@
-import { FC } from 'react'
+import {VFC} from 'react'
 import Link from 'next/link'
 import { Logo, Container } from '@components/ui'
 import { Searchbar, UserNav } from '@components/common'
 import NavbarRoot from './NavbarRoot'
 import s from './Navbar.module.css'
+import {useRouter} from "next/router";
 
-const Navbar: FC = () => (
-  <NavbarRoot>
-    <Container>
-      <div className="relative flex flex-row justify-between py-4 align-center">
-        <div className="flex items-center flex-1">
-          <Link href="/">
-            <a className={s.logo} aria-label="Logo">
-              <Logo />
-            </a>
-          </Link>
-          <nav className="hidden ml-6 space-x-4 lg:block">
-            <Link href="/search">
-              <a className={s.link}>All</a>
+const Navbar: VFC = () => {
+  const router = useRouter()
+
+  const menuItemClassName = (pathname: string, query: string, target: string | undefined): string => {
+    return `${s.link} ${router.pathname == `/${pathname}` && router.query[query] == target ? s.active : ''}`
+  }
+
+  return(
+    <NavbarRoot>
+      <Container>
+        <div className="relative flex flex-row justify-between py-4 align-center">
+          <div className="flex items-center flex-1">
+            <Link href="/">
+              <a className={s.logo} aria-label="Logo">
+                <Logo />
+              </a>
             </Link>
-            <Link href="/search?q=clothes">
-              <a className={s.link}>Clothes</a>
-            </Link>
-            <Link href="/search?q=accessories">
-              <a className={s.link}>Accessories</a>
-            </Link>
-            <Link href="/search?q=shoes">
-              <a className={s.link}>Shoes</a>
-            </Link>
-          </nav>
+            <nav className="hidden ml-6 space-x-4 lg:block">
+              <Link href={"/search?sort=latest-desc"}>
+                <a className={menuItemClassName('search','q', undefined)}>All</a>
+              </Link>
+              <Link href={"/search?q=clothes"}>
+                <a className={menuItemClassName('search','q', 'clothes')}>Clothes</a>
+              </Link>
+              <Link href={"/search?q=accessories"}>
+                <a className={menuItemClassName('search','q', 'accessories')}>Accessories</a>
+              </Link>
+              <Link href={"/search?q=shoes"}>
+                <a className={menuItemClassName('search','q', 'shoes')}>Shoes</a>
+              </Link>
+            </nav>
+          </div>
+
+          <div className="justify-center flex-1 hidden lg:flex">
+            <Searchbar />
+          </div>
+
+          <div className="flex justify-end flex-1 space-x-8">
+            <UserNav />
+          </div>
         </div>
 
-        <div className="justify-center flex-1 hidden lg:flex">
-          <Searchbar />
+        <div className="flex pb-4 lg:px-6 lg:hidden">
+          <Searchbar id="mobile-search" />
         </div>
-
-        <div className="flex justify-end flex-1 space-x-8">
-          <UserNav />
-        </div>
-      </div>
-
-      <div className="flex pb-4 lg:px-6 lg:hidden">
-        <Searchbar id="mobile-search" />
-      </div>
-    </Container>
-  </NavbarRoot>
-)
+      </Container>
+    </NavbarRoot>
+  )
+}
 
 export default Navbar
