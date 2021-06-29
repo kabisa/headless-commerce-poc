a80703c464c8f99a5103f6f756519a637afcd349
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-06-29 17:20:11 +0200

    Added product recommendation based on past 3 orders brands

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import commerce from '@lib/api/commerce'
 import { Layout } from '@components/common'
 import HomeAllProductsGrid from '@components/common/HomeAllProductsGrid'
-import type {GetStaticPropsContext, InferGetStaticPropsType} from 'next'
+import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
+import { useCustomerOrders } from "@framework/customer";
+import { OrderEdge, OrderLineItemEdge } from "@framework/schema";
+import React, { useEffect, useState } from "react";
+import useSearch from "@framework/product/use-search";
+import _ from "lodash";
+import { Product } from "@commerce/types/product";
+import { Customer } from "@commerce/types/customer";
 
 export async function getStaticProps({
   preview,
@@ -30,6 +37,7 @@ export async function getStaticProps({
       categories,
       brands,
       pages,
+      locale,
     },
     revalidate: 60,
   }
@@ -38,10 +46,42 @@ export async function getStaticProps({
 export default function Home({
   products,
   categories,
-  brands
+  brands,
+  locale
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const initialValue: string[] = []
+  const [orderedBrands, setOrderedBrands] = useState<string[]>(initialValue)
+  let recommendedProduct: Product | undefined
+
+  const { data: customerOrders } = useCustomerOrders({ numberOfOrders: 3 } ) as Customer // Get customer orders
+
+  const { data: recommendedProducts } = useSearch({ // Get products based on random ordered brand
+    brandId: _.sample(orderedBrands),
+    locale,
+  })
+
+  useEffect(() => {
+    customerOrders?.orders.edges.map((order: OrderEdge) => { // Iterate through orders and brands
+      order.node.lineItems.edges.map((item: OrderLineItemEdge) => {
+        if (item.node.variant?.product.vendor && orderedBrands.indexOf(item.node.variant?.product.vendor) === -1) {
+          initialValue.push(item.node.variant?.product.vendor) // Collect ordered brands
+          setOrderedBrands(initialValue)
+        }
+      })
+    })
+
+    recommendedProduct = _.sample(recommendedProducts?.products) // Take random recommended product from brand
+    if (recommendedProduct) {
+      const result = products.findIndex(product => { return product.id === recommendedProduct?.id }) // Check if recommended product already exists in list of products
+      if (result != -1) { products.splice(result, 1) } // If it exists remove it
+      recommendedProduct.name = `FOR YOU! ${recommendedProduct.name}` // Alter name of recommended product
+      products.unshift(recommendedProduct) // Add recommended product to beginning of list
+    }
+  }, [customerOrders])
+
   return (
     <>
+      {/*Commented because of alternate homepage layout*/}
       {/*<Grid variant="filled">*/}
       {/*  {products.slice(0, 3).map((product: any, i: number) => (*/}
       {/*    <ProductCard*/}
