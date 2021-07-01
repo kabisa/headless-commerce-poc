ce7d2f152f9f7b1d60242160a72183955b7211ea
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-07-01 17:07:15 +0200

    Added API interception to login function

--- cypress/integration/helpers/login.js
+++ cypress/integration/helpers/login.js
@@ -6,8 +6,15 @@ export default function login(credentials = { email, password }) {
   cy.findByPlaceholderText('Email').click().type(credentials.email)
   cy.findByPlaceholderText('Password').click().type(credentials.password)
   cy.findByText('Log In').click()
+
+  cy.wait('@customerAccessTokenCreateMutation').then(interception => {
+    assert.isNotNull(interception.response.body, 'customerAccessTokenCreateMutation API call has data')
+  })
+
+  cy.wait('@getCustomerQuery').then(interception => {
+    assert.isNotNull(interception.response.body, 'getCustomerQuery API call has data')
+  })
+
   cy.waitUntil(() => cy.findByText("Successfully logged in!"))
   cy.findByText("Successfully logged in!").should("not.exist")
 }
-
-/* global cy */
