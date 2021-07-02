0552f233909d879881160c5fd3560154a02ed1fa
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-07-02 15:59:03 +0200

    Added test utilities for GraphQL request intercepting

--- cypress/integration/helpers/graphql-test-utils.js
+++ cypress/integration/helpers/graphql-test-utils.js
@@ -25,3 +25,36 @@ export const aliasMutation = (req, operationName) => {
     req.alias = `${operationName}Mutation`
   }
 }
+
+export const  captureRequests = (operations, method = 'POST', matcher = '**/graphql.json') => { // Used in the beforeEach hook in cypress tests to capture all requests and alias them
+  cy.intercept(method, matcher, (req) => { // GraphQL Queries and mutations to intercept in this test
+    console.log(req); // Log all graphql requests made
+    for (const key in operations) { // Iterate through operations
+      for (const value in operations[key]) { // iterate though queries and mutations
+        if (key === 'Query') { // For queries in operations
+          aliasQuery(req, operations[key][value])
+        } else if (key === 'Mutation') { // For mutations in operations
+          aliasMutation(req, operations[key][value])
+        }
+      }
+    }
+  })
+};
+
+export const interceptRequests = (operations, method = 'POST', matcher = '**/graphql.json') => { // Used in beginning of the test to intercept requests and
+  cy.intercept(method, matcher, (req) => { // Intercept all GraphQL queries
+    console.log(req); // Log all graphql requests made
+    for (const key in operations) {
+      for (const value in operations[key]) {
+        if (hasOperationName(req, operations[key][value])) {
+          req.alias = `${operations[key][value]}${key}` // Declare the alias from the initial intercept in the beforeEach
+          req.reply({ // Modify response with mock data
+              fixture: `${operations[key][value]}Data.json` || {}, // Fixture data file name in /cypress/fixtures folder
+              delay: 500,
+            }
+          )
+        }
+      }
+    }
+  })
+};
