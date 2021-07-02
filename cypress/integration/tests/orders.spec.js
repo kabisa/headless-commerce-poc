b5cea968371f614dcf443e47719fb3f13e6b7858
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-07-02 15:59:50 +0200

    Modified tests to use mocked data

--- cypress/integration/tests/orders.spec.js
+++ cypress/integration/tests/orders.spec.js
@@ -1,65 +1,38 @@
 /// <reference types="cypress" />
 import login from "../helpers/login";
-import { aliasMutation, aliasQuery, hasOperationName } from "../helpers/graphql-test-utils";
-import customerAccessTokenCreateData from "../../fixtures/customerAccessTokenCreateData";
-import getCustomerData from "../../fixtures/getCustomerData";
-import getCustomerOrdersData from "../../fixtures/getCustomerOrdersData";
-import credentials from "../../fixtures/loginCredentials"
-
-const mockData = { // Define mockData to use for this test
-  customerAccessTokenCreateData: customerAccessTokenCreateData,
-  getCustomerData: getCustomerData,
-  getCustomerOrdersData: getCustomerOrdersData
-}
+import { captureRequests, interceptRequests } from "../helpers/graphql-test-utils";
+import credentials from "../../fixtures/loginCredentials";
+import { constants } from "../helpers/constants";
 
 const operations = { // Define GraphQL operations to intercept for this test (And differentiate between queries and mutations)
   Query: {
-    getCustomer: 'getCustomer',
-    Orders: 'getCustomerOrders'
+    orders: 'getCustomerOrders',
+    products: 'getProducts'
   },
   Mutation: {
-    customerAccessTokenCreate: 'customerAccessTokenCreate',
+    checkoutCreate: 'checkoutCreate',
   }
 }
 
 describe('Retrieve orders', () => {
   beforeEach(() => {
-    cy.intercept('POST', '**/graphql.json', (req) => { // GraphQL Queries and mutations to intercept in this test
-      for (const key in operations) { // Iterate through operations
-        for (const value in operations[key]) { // iterate though queries and mutations
-          if (key === 'queries') { // For queries in operations
-            aliasQuery(req, operations[key][value])
-          } else if (key === 'mutations') { // For mutations in operations
-            aliasMutation(req, operations[key][value])
-          }
-        }
-      }
-    })
+    captureRequests(operations)
 
     cy.visit('/') // Uses baseUrl: 'http://localhost:3000' Run project with `yarn run build` & `yarn run start`
   })
 
   it('Logs in user with orders and assert orders in dom', () => {
-    cy.intercept('POST', '**/graphql.json', (req) => { // Intercept all GraphQL queries
-      for (const key in operations) {
-        for (const value in operations[key]) {
-          if (hasOperationName(req, operations[key][value])) {
-            req.alias = `${operations[key][value]}${key}` // Declare the alias from the initial intercept in the beforeEach
-            req.reply( (res) => {
-              res.body = mockData[`${operations[key][value]}Data`] // Modify the response body directly with mock mockData
-              console.log(res.body);
-            })
-          }
-        }
-      }
+    interceptRequests(operations);
+
+    cy.wait('@checkoutCreateMutation').then(interception => {
+      assert.isNotNull(interception.response.body, 'checkoutCreateQuery API call has mockData')
     })
 
-    cy.wait(250)
-    cy.findByText('Accept cookies').click()
+    constants.dismissCookies()
 
     login(credentials)
 
-    cy.get('button[aria-label="Menu"]').click()
+    cy.get(constants.menuButton).click()
     cy.findByText('My Orders').click()
 
     cy.wait('@getCustomerOrdersQuery').then(interception => {
