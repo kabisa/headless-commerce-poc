598a9ba581bad843fec3b5994f95f47def9d2d42
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-07-02 15:59:16 +0200

    Stubbed login process

--- cypress/integration/helpers/login.js
+++ cypress/integration/helpers/login.js
@@ -1,8 +1,22 @@
 /// <reference types="cypress" />
 
-export default function login(credentials = { email, password }) {
+import { interceptRequests } from "./graphql-test-utils";
+import { constants } from "./constants";
 
-  cy.get('.UserNav_avatarButton__1O6kn').click()
+const operations = { // Define GraphQL operations to intercept for this test (And differentiate between queries and mutations)
+  Query: {
+    getCustomer: 'getCustomer',
+  },
+  Mutation: {
+    customerAccessTokenCreate: 'customerAccessTokenCreate',
+  }
+}
+
+export default function login(credentials = {email, password}) {
+
+  interceptRequests(operations);
+
+  cy.get(constants.menuButton).click()
   cy.findByPlaceholderText('Email').click().type(credentials.email)
   cy.findByPlaceholderText('Password').click().type(credentials.password)
   cy.findByText('Log In').click()
