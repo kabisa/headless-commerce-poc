1f38aadf1097b272cae482370c77eca6774dfd34
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-07-06 11:06:51 +0200

    Remove curly braces from href attribute string

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -32,7 +32,7 @@ const Footer: FC<Props> = ({ className, pages }) => {
       <Container>
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 border-b border-accent-2 py-12 text-primary bg-primary transition-colors duration-150">
           <div className="col-span-1 lg:col-span-2">
-            <Link href={"https://kabisa.nl/"}>
+            <Link href="https://kabisa.nl/">
               <a className="flex flex-initial items-center font-bold md:mr-24">
                 <span className="mr-2">
                   <Logo />
