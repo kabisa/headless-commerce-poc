94ea9cb64f7805c1e46d88b836c7033f52c50c98
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-07-07 14:50:49 +0200

    Extracted sortButton styles

--- components/search/Search.tsx
+++ components/search/Search.tsx
@@ -76,7 +76,7 @@ export default function Search({ categories, brands }: SearchPropsType) {
                 <button
                   type="button"
                   onClick={(e) => handleClick(e, 'categories')}
-                  className="flex justify-between w-full rounded-sm border border-accent-3 px-4 py-3 bg-accent-0 text-sm leading-5 font-medium text-accent-4 hover:text-accent-5 focus:outline-none focus:border-blue-300 focus:shadow-outline-normal active:bg-accent-1 active:text-accent-8 transition ease-in-out duration-150"
+                  className={s.sortButton}
                   id="options-menu"
                   aria-haspopup="true"
                   aria-expanded="true">
@@ -119,7 +119,7 @@ export default function Search({ categories, brands }: SearchPropsType) {
                 <button
                   type="button"
                   onClick={(e) => handleClick(e, 'brands')}
-                  className="flex justify-between w-full rounded-sm border border-accent-3 px-4 py-3 bg-accent-0 text-sm leading-5 font-medium text-accent-8 hover:text-accent-5 focus:outline-none focus:border-blue-300 focus:shadow-outline-normal active:bg-accent-1 active:text-accent-8 transition ease-in-out duration-150"
+                  className={s.sortButton}
                   id="options-menu"
                   aria-haspopup="true"
                   aria-expanded="true">
@@ -215,7 +215,7 @@ export default function Search({ categories, brands }: SearchPropsType) {
                 <button
                   type="button"
                   onClick={(e) => handleClick(e, 'sort')}
-                  className="flex justify-between w-full rounded-sm border border-accent-3 px-4 py-3 bg-accent-0 text-sm leading-5 font-medium text-accent-4 hover:text-accent-5 focus:outline-none focus:border-blue-300 focus:shadow-outline-normal active:bg-accent-1 active:text-accent-8 transition ease-in-out duration-150"
+                  className={s.sortButton}
                   id="options-menu"
                   aria-haspopup="true"
                   aria-expanded="true">
