ed85956470ab0c4a6774308fe99b8a15265c905b
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-07-07 11:53:24 +0200

    Cleanup code

--- components/search/Search.tsx
+++ components/search/Search.tsx
@@ -8,6 +8,7 @@ import { Layout } from '@components/common'
 import { ProductCard } from '@components/product'
 import type { Product } from '@commerce/types/product'
 import { Container, Skeleton } from '@components/ui'
+import s from './Search.module.css'
 
 import useSearch from '@framework/product/use-search'
 
@@ -77,85 +78,44 @@ export default function Search({ categories, brands }: SearchPropsType) {
                   className="flex justify-between w-full rounded-sm border border-accent-3 px-4 py-3 bg-accent-0 text-sm leading-5 font-medium text-accent-4 hover:text-accent-5 focus:outline-none focus:border-blue-300 focus:shadow-outline-normal active:bg-accent-1 active:text-accent-8 transition ease-in-out duration-150"
                   id="options-menu"
                   aria-haspopup="true"
-                  aria-expanded="true"
-                >
-                  {activeCategory?.name
-                    ? `Category: ${activeCategory?.name}`
-                    : 'All Categories'}
+                  aria-expanded="true">
+                  {activeCategory?.name ? `Category: ${activeCategory?.name}` : 'All Categories'}
                   <svg
                     className="-mr-1 ml-2 h-5 w-5"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
+                    fill="currentColor">
                     <path
                       fillRule="evenodd"
                       d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
+                      clipRule="evenodd"/>
                   </svg>
                 </button>
               </span>
             </div>
             <div
               className={`origin-top-left absolute lg:relative left-0 mt-2 w-full rounded-md shadow-lg lg:shadow-none z-10 mb-10 lg:block ${
-                activeFilter !== 'categories' || !toggleFilter
-                  ? 'hidden'
-                  : ''
-              }`}
-            >
+                activeFilter !== 'categories' || !toggleFilter ? 'hidden' : ''}`}>
               <div className="rounded-sm bg-accent-0 shadow-xs lg:bg-none lg:shadow-none">
                 <div
                   role="menu"
                   aria-orientation="vertical"
-                  aria-labelledby="options-menu"
-                >
+                  aria-labelledby="options-menu">
                   <ul>
-                    <li
-                      className={cn(
-                        'block text-sm leading-5 text-accent-4 lg:text-base lg:no-underline lg:font-bold lg:tracking-wide hover:bg-accent-1 lg:hover:bg-transparent hover:text-accent-8 focus:outline-none focus:bg-accent-1 focus:text-accent-8',
-                        {
-                          underline: !activeCategory?.name,
-                        }
-                      )}
-                    >
-                      <Link
-                        href={{ pathname: getCategoryPath('', brand), query }}
-                      >
-                        <a
-                          onClick={(e) => handleClick(e, 'categories')}
-                          className={
-                            'block lg:inline-block px-4 py-2 lg:p-0 lg:my-2 lg:mx-4'
-                          }
-                        >
-                          All Categories
-                        </a>
+                    <li className={cn('block text-sm leading-5 text-accent-4 lg:text-base lg:no-underline lg:font-bold lg:tracking-wide hover:bg-accent-1 lg:hover:bg-transparent hover:text-accent-8 focus:outline-none focus:bg-accent-1 focus:text-accent-8',
+                        { underline: !activeCategory?.name, } )} >
+                      <Link href={{ pathname: getCategoryPath('', brand), query }}>
+                        <a onClick={(e) => handleClick(e, 'categories')}
+                          className={ 'block lg:inline-block px-4 py-2 lg:p-0 lg:my-2 lg:mx-4' } > All Categories</a>
                       </Link>
                     </li>
                     {categories.map((cat: any) => (
-                      <li
-                        key={cat.path}
-                        className={cn(
-                          'block text-sm leading-5 text-accent-4 hover:bg-accent-1 lg:hover:bg-transparent hover:text-accent-8 focus:outline-none focus:bg-accent-1 focus:text-accent-8',
-                          {
-                            underline: activeCategory?.id === cat.id,
-                          }
-                        )}
-                      >
+                      <li key={cat.path} className={cn(s.listItem, { underline: activeCategory?.id === cat.id, } )}>
                         <Link
-                          href={{
-                            pathname: getCategoryPath(cat.path, brand),
-                            query,
-                          }}
-                        >
-                          <a
-                            onClick={(e) => handleClick(e, 'categories')}
-                            className={
-                              'block lg:inline-block px-4 py-2 lg:p-0 lg:my-2 lg:mx-4'
-                            }
-                          >
-                            {cat.name}
-                          </a>
+                          href={{ pathname: getCategoryPath(cat.path, brand), query,}}>
+                          <a onClick={(e) => handleClick(e, 'categories')}
+                            className={'block lg:inline-block px-4 py-2 lg:p-0 lg:my-2 lg:mx-4'}>
+                            {cat.name}</a>
                         </Link>
                       </li>
                     ))}
@@ -175,22 +135,17 @@ export default function Search({ categories, brands }: SearchPropsType) {
                   className="flex justify-between w-full rounded-sm border border-accent-3 px-4 py-3 bg-accent-0 text-sm leading-5 font-medium text-accent-8 hover:text-accent-5 focus:outline-none focus:border-blue-300 focus:shadow-outline-normal active:bg-accent-1 active:text-accent-8 transition ease-in-out duration-150"
                   id="options-menu"
                   aria-haspopup="true"
-                  aria-expanded="true"
-                >
-                  {activeBrand?.name
-                    ? `Design: ${activeBrand?.name}`
-                    : 'All Designs'}
+                  aria-expanded="true">
+                  {activeBrand?.name ? `Design: ${activeBrand?.name}` : 'All Designs'}
                   <svg
                     className="-mr-1 ml-2 h-5 w-5"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
+                    fill="currentColor">
                     <path
                       fillRule="evenodd"
                       d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
+                      clipRule="evenodd"/>
                   </svg>
                 </button>
               </span>
@@ -200,14 +155,12 @@ export default function Search({ categories, brands }: SearchPropsType) {
                 activeFilter !== 'brands' || !toggleFilter
                   ? 'hidden'
                   : ''
-              }`}
-            >
+              }`}>
               <div className="rounded-sm bg-accent-0 shadow-xs lg:bg-none lg:shadow-none">
                 <div
                   role="menu"
                   aria-orientation="vertical"
-                  aria-labelledby="options-menu"
-                >
+                  aria-labelledby="options-menu">
                   <ul>
                     <li
                       className={cn(
@@ -215,49 +168,17 @@ export default function Search({ categories, brands }: SearchPropsType) {
                         {
                           underline: !activeBrand?.name,
                         }
-                      )}
-                    >
-                      <Link
-                        href={{
-                          pathname: getDesignerPath('', category),
-                          query,
-                        }}
-                      >
-                        <a
-                          onClick={(e) => handleClick(e, 'brands')}
-                          className={
-                            'block lg:inline-block px-4 py-2 lg:p-0 lg:my-2 lg:mx-4'
-                          }
-                        >
-                          All Designers
-                        </a>
+                      )}>
+                      <Link href={{pathname: getDesignerPath('', category), query,}}>
+                        <a onClick={(e) => handleClick(e, 'brands')}
+                          className={'block lg:inline-block px-4 py-2 lg:p-0 lg:my-2 lg:mx-4'}>All Designers</a>
                       </Link>
                     </li>
                     {brands.flatMap(({ node }: { node: any }) => (
-                      <li
-                        key={node.path}
-                        className={cn(
-                          'block text-sm leading-5 text-accent-4 hover:bg-accent-1 lg:hover:bg-transparent hover:text-accent-8 focus:outline-none focus:bg-accent-1 focus:text-accent-8',
-                          {
-                            // @ts-ignore Shopify - Fix this types
-                            underline: activeBrand?.entityId === node.entityId,
-                          }
-                        )}
-                      >
-                        <Link
-                          href={{
-                            pathname: getDesignerPath(node.path, category),
-                            query,
-                          }}
-                        >
-                          <a
-                            onClick={(e) => handleClick(e, 'brands')}
-                            className={
-                              'block lg:inline-block px-4 py-2 lg:p-0 lg:my-2 lg:mx-4'
-                            }
-                          >
-                            {node.name}
-                          </a>
+                      <li key={node.path} className={cn(s.listItem, {underline: activeBrand?.entityId === node.entityId,})}>
+                        <Link href={{pathname: getDesignerPath(node.path, category), query,}}>
+                          <a onClick={(e) => handleClick(e, 'brands')}
+                            className={'block lg:inline-block px-4 py-2 lg:p-0 lg:my-2 lg:mx-4'}>{node.name}</a>
                         </Link>
                       </li>
                     ))}
@@ -277,8 +198,7 @@ export default function Search({ categories, brands }: SearchPropsType) {
                     className={cn('animated', {
                       fadeIn: data.found,
                       hidden: !data.found,
-                    })}
-                  >
+                    })}>
                     Showing {data.products.length} results{' '}
                     {q && (
                       <>
@@ -290,23 +210,16 @@ export default function Search({ categories, brands }: SearchPropsType) {
                     className={cn('animated', {
                       fadeIn: !data.found,
                       hidden: data.found,
-                    })}
-                  >
+                    })}>
                     {q ? (
-                      <>
-                        There are no products that match &quot;<strong>{q}</strong>&quot;
-                      </>
+                      <>There are no products that match &quot;<strong>{q}</strong>&quot;</>
                     ) : (
-                      <>
-                        There are no products that match the selected category.
-                      </>
+                      <>There are no products that match the selected category.</>
                     )}
                   </span>
                 </>
               ) : q ? (
-                <>
-                  Searching for: &quot;<strong>{q}</strong>&quot;
-                </>
+                <>Searching for: &quot;<strong>{q}</strong>&quot;</>
               ) : (
                 <>Searching...</>
               )}
@@ -323,8 +236,7 @@ export default function Search({ categories, brands }: SearchPropsType) {
                   imgProps={{
                     width: 480,
                     height: 480,
-                  }}
-                />
+                  }}/>
               ))}
             </div>
           ) : (
@@ -349,20 +261,17 @@ export default function Search({ categories, brands }: SearchPropsType) {
                   className="flex justify-between w-full rounded-sm border border-accent-3 px-4 py-3 bg-accent-0 text-sm leading-5 font-medium text-accent-4 hover:text-accent-5 focus:outline-none focus:border-blue-300 focus:shadow-outline-normal active:bg-accent-1 active:text-accent-8 transition ease-in-out duration-150"
                   id="options-menu"
                   aria-haspopup="true"
-                  aria-expanded="true"
-                >
+                  aria-expanded="true">
                   {sort ? `Sort: ${sort.toString()}` : 'Relevance'}
                   <svg
                     className="-mr-1 ml-2 h-5 w-5"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
+                    fill="currentColor">
                     <path
                       fillRule="evenodd"
                       d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
+                      clipRule="evenodd"/>
                   </svg>
                 </button>
               </span>
@@ -370,58 +279,28 @@ export default function Search({ categories, brands }: SearchPropsType) {
             <div
               className={`origin-top-left absolute lg:relative left-0 mt-2 w-full rounded-md shadow-lg lg:shadow-none z-10 mb-10 lg:block ${
                 activeFilter !== 'sort' || toggleFilter !== true ? 'hidden' : ''
-              }`}
-            >
+              }`}>
               <div className="rounded-sm bg-accent-0 shadow-xs lg:bg-none lg:shadow-none">
                 <div
                   role="menu"
                   aria-orientation="vertical"
-                  aria-labelledby="options-menu"
-                >
+                  aria-labelledby="options-menu">
                   <ul>
                     <li
-                      className={cn(
-                        'block text-sm leading-5 text-accent-4 lg:text-base lg:no-underline lg:font-bold lg:tracking-wide hover:bg-accent-1 lg:hover:bg-transparent hover:text-accent-8 focus:outline-none focus:bg-accent-1 focus:text-accent-8',
-                        {
-                          underline: !sort,
-                        }
-                      )}
-                    >
+                      className={cn('block text-sm leading-5 text-accent-4 lg:text-base lg:no-underline lg:font-bold lg:tracking-wide hover:bg-accent-1 lg:hover:bg-transparent hover:text-accent-8 focus:outline-none focus:bg-accent-1 focus:text-accent-8',
+                        {underline: !sort,})}>
                       <Link href={{ pathname, query: filterQuery({ q }) }}>
                         <a
                           onClick={(e) => handleClick(e, 'sort')}
-                          className={
-                            'block lg:inline-block px-4 py-2 lg:p-0 lg:my-2 lg:mx-4'
-                          }
-                        >
-                          Relevance
-                        </a>
+                          className={'block lg:inline-block px-4 py-2 lg:p-0 lg:my-2 lg:mx-4'}>Relevance</a>
                       </Link>
                     </li>
                     {SORT.map(([key, text]) => (
-                      <li
-                        key={key}
-                        className={cn(
-                          'block text-sm leading-5 text-accent-4 hover:bg-accent-1 lg:hover:bg-transparent hover:text-accent-8 focus:outline-none focus:bg-accent-1 focus:text-accent-8',
-                          {
-                            underline: sort === key,
-                          }
-                        )}
-                      >
+                      <li key={key} className={cn(s.listItem, { underline: sort === key, })}>
                         <Link
-                          href={{
-                            pathname,
-                            query: filterQuery({ q, sort: key }),
-                          }}
-                        >
-                          <a
-                            onClick={(e) => handleClick(e, 'sort')}
-                            className={
-                              'block lg:inline-block px-4 py-2 lg:p-0 lg:my-2 lg:mx-4'
-                            }
-                          >
-                            {text}
-                          </a>
+                          href={{pathname, query: filterQuery({ q, sort: key }),}}>
+                          <a onClick={(e) => handleClick(e, 'sort')}
+                            className={'block lg:inline-block px-4 py-2 lg:p-0 lg:my-2 lg:mx-4'}>{text}</a>
                         </Link>
                       </li>
                     ))}
