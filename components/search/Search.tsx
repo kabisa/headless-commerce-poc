9d743066bb3c5761be3ce1c1bee5fa1f252383fc
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-07-15 18:29:34 +0200

    Restore

--- components/search/Search.tsx
+++ components/search/Search.tsx
@@ -180,19 +180,19 @@ export default function Search({ categories, brands }: SearchPropsType) {
               )}
             </div>
           )}
-          {!data ? (
+          {data ? (
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-              {/*{data.products.map((product) => (*/}
-              {/*  <ProductCard*/}
-              {/*    variant="simple"*/}
-              {/*    key={product.path}*/}
-              {/*    className="animated fadeIn"*/}
-              {/*    product={product}*/}
-              {/*    imgProps={{*/}
-              {/*      width: 480,*/}
-              {/*      height: 480,*/}
-              {/*    }}/>*/}
-              {/*))}*/}
+              {data.products.map((product) => (
+                <ProductCard
+                  variant="simple"
+                  key={product.path}
+                  className="animated fadeIn"
+                  product={product}
+                  imgProps={{
+                    width: 480,
+                    height: 480,
+                  }}/>
+              ))}
             </div>
           ) : (
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
