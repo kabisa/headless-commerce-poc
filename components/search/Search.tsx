b227770609342fa6f5f1460f1445aba1e2a42693
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-07-15 18:35:34 +0200

    Skeleton style opacity

--- components/search/Search.tsx
+++ components/search/Search.tsx
@@ -195,7 +195,7 @@ export default function Search({ categories, brands }: SearchPropsType) {
               ))}
             </div>
           ) : (
-            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 opacity-50">
               {rangeMap(12, (i) => (
                 <Skeleton key={i}>
                   <div className="w-60 h-60" />
